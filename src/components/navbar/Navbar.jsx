522e5b640df14d220e3dca5dde720a964de4867e
Henok-Al <henok.gebresenbet@gmail.com>
2024-10-19 20:41:52 -0400

    topnavbar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar";
 import { useSelector } from "react-redux";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaShoppingCart } from "react-icons/fa";
+
+// TopNavbar Component
+const TopNavbar = () => {
+  return (
+    <div className="bg-gray-900 text-white text-sm py-1 text-center">
+      <p>Call us: +251928769996</p>
+    </div>
+  );
+};
 
 const Navbar = () => {
   // get user from localStorage
@@ -100,49 +109,59 @@ const Navbar = () => {
       {/* Cart */}
       <li>
         <Link to={"/cart"} onClick={() => setIsOpen(false)}>
-          Cart({cartItems.length})
+          <div className="relative inline-block">
+            <FaShoppingCart className="text-white text-2xl" />
+            {cartItems.length > 0 && (
+              <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-bold rounded-full w-4 h-4 flex items-center justify-center">
+                {cartItems.length}
+              </span>
+            )}
+          </div>
         </Link>
       </li>
     </ul>
   );
 
   return (
-    <nav className="bg-orange-600 sticky top-0">
-      {/* main  */}
-      <div className="lg:flex lg:justify-between items-center py-3 lg:px-3">
-        {/* left  */}
-        <div className="left py-3 lg:py-0">
-          <Link to={"/"}>
-            <h2 className=" font-bold text-white text-2xl text-center">
-              YebaleSpices
-            </h2>
-          </Link>
-        </div>
+    <>
+      <TopNavbar />
+      <nav className="bg-orange-600 sticky top-0 z-50">
+        {/* main  */}
+        <div className="lg:flex lg:justify-between items-center py-3 lg:px-3">
+          {/* left  */}
+          <div className="left py-3 lg:py-0">
+            <Link to={"/"}>
+              <h2 className=" font-bold text-white text-2xl text-center">
+                YebaleSpices
+              </h2>
+            </Link>
+          </div>
 
-        {/* Hamburger Menu */}
-        <div className="lg:hidden absolute top-4 right-4">
-          <button onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? (
-              <FaTimes className="text-white text-2xl" />
-            ) : (
-              <FaBars className="text-white text-2xl" />
-            )}
-          </button>
-        </div>
+          {/* Hamburger Menu */}
+          <div className="lg:hidden absolute top-4 right-4">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? (
+                <FaTimes className="text-white text-2xl" />
+              ) : (
+                <FaBars className="text-white text-2xl" />
+              )}
+            </button>
+          </div>
 
-        {/* right  */}
-        <div
-          className={`right flex flex-col items-center mt-4 lg:mt-0 lg:flex-row lg:justify-center lg:space-x-3 ${
-            isOpen ? "block" : "hidden"
-          } lg:block`}
-        >
-          {navList}
-        </div>
+          {/* right  */}
+          <div
+            className={`right flex flex-col items-center mt-4 lg:mt-0 lg:flex-row lg:justify-center lg:space-x-3 ${
+              isOpen ? "block" : "hidden"
+            } lg:block`}
+          >
+            {navList}
+          </div>
 
-        {/* Search Bar  */}
-        <SearchBar />
-      </div>
-    </nav>
+          {/* Search Bar  */}
+          <SearchBar />
+        </div>
+      </nav>
+    </>
   );
 };
 
